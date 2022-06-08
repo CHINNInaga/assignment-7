ce740ca89aac52e343ff854646c4a58c7eb827db
bhuvana <104904048+CHINNInaga@users.noreply.github.com>
2022-06-08 18:50:52 +0530

    Add files via upload

---
+++ GateWay/src/main/java/com/Store_locator/GateWayApplication.java
@@ -0,0 +1,14 @@
+package com.Store_locator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
---
+++ GateWay/src/test/java/com/Store_locator/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Store_locator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
